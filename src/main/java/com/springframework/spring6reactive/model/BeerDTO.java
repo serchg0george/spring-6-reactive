7c93b73ae35206a1c4417ac4a025d14a4395a7c2
serchg0george <serchg0george@gmail.com>
2023-07-25 13:54:21 +0300

    Implement simple data validation

--- src/main/java/com/springframework/spring6reactive/model/BeerDTO.java
+++ src/main/java/com/springframework/spring6reactive/model/BeerDTO.java
@@ -1,5 +1,7 @@
 package com.springframework.spring6reactive.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,8 +17,16 @@ import java.time.LocalDateTime;
 public class BeerDTO {
 
     private Integer id;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
     private String beerName;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
     private String beerStyle;
+
+    @Size(max = 25)
     private String upc;
     private Integer quantityOnHand;
     private BigDecimal price;
