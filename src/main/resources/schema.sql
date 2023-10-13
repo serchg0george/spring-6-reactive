1828254a26c84158b82fb4cc8cb22d3a8fed2f57
serchg0george <serchg0george@gmail.com>
2023-10-13 14:42:16 +0300

    Fixed SQL schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,5 @@
 CREATE TABLE if NOT EXISTS beer
 (
-    id             integer NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    beer_name      varchar(255),
-    beer_style     varchar (255),
-    upc            varchar (25),
-    quantity_on_hand integer,
-    price          decimal,
-    created_date   timestamp,
     id                 integer NOT NULL PRIMARY KEY AUTO_INCREMENT,
     beer_name          varchar(255),
     beer_style         varchar(255),
